19cc3af3962becf68f85378b3920f32453f58d3e
Mohammed Karishma <2300040100ece@gmail.com>
2026-05-07 11:11:18 +0530

    Add render config for deployment

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'expense-tracker-secret-key'
@@ -126,4 +127,4 @@ def delete_expense(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
