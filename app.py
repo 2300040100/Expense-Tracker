1eda5541f1180eb3b4ec222cf6ff84366632ef00
Mohammed Karishma <2300040100ece@gmail.com>
2026-05-07 12:10:12 +0530

    Switch to PostgreSQL

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'expense-tracker-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///expenses.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
